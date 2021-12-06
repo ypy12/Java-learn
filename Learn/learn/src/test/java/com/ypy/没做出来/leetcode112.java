e732cd26f11d918a700077973093f08ac780b311
ypy121 <1052276894@qq.com>
2021-12-06 13:49:31 +0800

    增加12月6日

---
+++ Learn/learn/src/test/java/com/ypy/没做出来/leetcode112.java
@@ -0,0 +1,53 @@
+/*
+ * @Description: 
+ * @Author: YPY
+ * @Date: 2021-12-06 11:37:59
+ * @LastEditTime: 2021-12-06 13:49:26
+ * @LastEditors: YPY
+ * @Reference: 
+ */
+package com.ypy.没做出来;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import com.ypy.suanfa.TreeNode;
+
+public class leetcode112 {
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Queue<Integer> sum = new LinkedList<Integer>();
+        stack.add(root);
+        sum.add(root.val);
+
+        while (!stack.isEmpty()) {
+            int targetSum
+
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode left = new TreeNode(4, null, new TreeNode(3));
+        TreeNode right = new TreeNode(5, null, new TreeNode(31));
+        TreeNode root = new TreeNode(1, left, right);
+        boolean result = hasPathSum(root, 8);
+        System.out.println(result);
+    }
+}
+/*
+ * @Description:
+ * 
+ * @Author: YPY
+ * 
+ * @Date: 2021-12-06 11:37:59
+ * 
+ * @LastEditTime: 2021-12-06 11:37:59
+ * 
+ * @LastEditors: YPY
+ * 
+ * @Reference:
+ */
